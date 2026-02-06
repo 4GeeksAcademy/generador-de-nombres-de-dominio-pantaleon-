f22a958eca37d08f10b7932444343b6f83ecd4a8
pantaleonyy <yona.pantaleon48@icloud.com>
2026-02-06 17:30:41 +0000

    generador de nombres pantaleon

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+   for(let p of pronoun ){
+    for(let a of adj){
+      for(let n of noun){
+        let name = p + a + n 
+        let domain = name + ".com"
+         console.log(domain);
+       
+        
+
+      }
+    }
+
+   }
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  
 };
